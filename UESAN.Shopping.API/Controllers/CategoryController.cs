3347d89c57e0757b05b054673b31ea3b3396d7a1
Luis Chang <luis.chang@outlook.com>
2023-05-02 08:45:38 -0500

    CRUD en CategoryController

--- UESAN.Shopping.API/Controllers/CategoryController.cs
+++ UESAN.Shopping.API/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UESAN.Shopping.Core.Entities;
 using UESAN.Shopping.Core.Interfaces;
 
 namespace UESAN.Shopping.API.Controllers
@@ -22,5 +23,45 @@ namespace UESAN.Shopping.API.Controllers
             return Ok(categories);
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryRepository.GetById(id);
+            if(category==null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(Category category)
+        {
+           var result =  await _categoryRepository.Insert(category);
+           if(!result)
+                return BadRequest();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Category category)
+        {
+            if (id != category.Id)
+                return NotFound();
+
+            var result = await _categoryRepository.Update(category);
+            if(!result)
+                return BadRequest();
+            
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        { 
+            var result = await _categoryRepository.Delete(id);
+            if (!result)
+                return BadRequest();
+
+            return NoContent();
+        }
     }
 }
