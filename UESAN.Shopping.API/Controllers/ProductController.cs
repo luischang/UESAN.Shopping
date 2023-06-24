b5172ea2dd2b71d8326dbe223ebbafcba39a81ee
Luis Chang <luis.chang@outlook.com>
2023-06-24 07:40:05 -0500

    Added file image in created product

--- UESAN.Shopping.API/Controllers/ProductController.cs
+++ UESAN.Shopping.API/Controllers/ProductController.cs
@@ -32,12 +32,45 @@ namespace UESAN.Shopping.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Insert(ProductInsertDTO productDTO)
+        public async Task<IActionResult> Insert([FromForm] ProductInsertDTO productDTO, IFormFile file)
         {
-            var result = await _productService.Insert(productDTO);
-            if (!result)
-                return BadRequest(result);
-            return Ok(result);
+            try
+            {
+                if (file == null || file.Length == 0)
+                    return BadRequest("No se proporcionó ningún archivo.");
+
+                // Ruta de la carpeta donde se guardarán los archivos
+                var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "files");
+
+                // Si la carpeta no existe, se crea
+                if (!Directory.Exists(folderPath))
+                    Directory.CreateDirectory(folderPath);
+
+                // Generar un nombre único para el archivo
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                // Ruta completa del archivo
+                var filePath = Path.Combine(folderPath, fileName);
+
+                // Copiar el archivo al servidor
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                // Asignar la ruta del archivo al atributo ImageUrl del DTO del producto
+                productDTO.ImageUrl = fileName;
+
+                var result = await _productService.Insert(productDTO);
+                if (!result)
+                    return BadRequest(result);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         [HttpPut("{id}")]
